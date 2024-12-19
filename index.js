753455bc8599c487c738f9216cb17fbc12a3ca13
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2024-12-19 11:34:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,28 +84,8 @@ app.use("/doctor", doctorUser);
 app.use("/patient", patientUser);
 app.use("/alldoctors", allDoctors);
 app.use("/doctorprofile", doctorProfile);
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Hospital Management System ',
-      version: '1.0.0',
-      description: 'Hospital Management System API information for System analysis - 1 ',
-    },
-    servers: [
-      {
-        url: 'https://pleasant-comfort-production-f2ef.up.railway.app',
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // Define paths to files containing annotations
-};
-const spacs = swaggerJsDoc(options)
-app.use(
-  "/api-docs" ,
-  swaggerUi.serve ,
-  swaggerUi.setup(spacs)
-)
+
+
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
